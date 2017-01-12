2a60aebb5e115a4e653f35db5100d697bb0a6c40
awanderlyss <alyssa.felix7@gmail.com>
2017-01-12 14:49:44 -0500

    Set up node server in server.js file

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var hbs     = require('hbs');
+var app     = express();
+
+// Set hbs views engine(config)
+app.set('view engine', 'hbs');
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(3001, () => {
+  console.log('//********LISTENING ON PORT 3001********//');
+});
